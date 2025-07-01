1ef09405d2dc2c7193643721d3aafcd35496df5e
Shubhachand <shubhachandpatel2022@gmail.com>
2025-07-02 04:21:31 +0530

    final commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,7 @@ import authRoutes from "./routes/auth.route.js"
 import chatRoutes from "./routes/chat.route.js";
 import {connectDB} from "./config/db.js"
 import userRoutes from './routes/user.route.js'
+import path from "path";
 import cors from "cors";
 dotenv.config();
 const app = express();
@@ -21,7 +22,14 @@ app.use("/api/auth",authRoutes);
 app.use("/api/users",userRoutes)
 app.use("/api/chat",chatRoutes)
 
- 
+const __dirname = path.resolve();
+ if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`server is runnning on port ${PORT}`);
